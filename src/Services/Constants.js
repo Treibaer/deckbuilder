cc4c2f7cb25e6b954cdd25c27d357d6cc2772a9b
Hannes Kühnel <hanneskuehnel@gmx.de>
2024-07-13 18:56:33 +0200

    configs added

--- src/Services/Constants.js
+++ src/Services/Constants.js
@@ -1,16 +1,9 @@
 const prod = {
-  url: {
-    backendUrl: `https://m2.treibaer.de`,
-    loginApiHost: `https://n2.treibaer.de`,
-  },
+  backendUrl: `https://rt.treibaer.de`,
 };
 
 const dev = {
   backendUrl: `https://magic.treibaer.de`,
-  loginApiHost: `https://magic.treibaer.de`,
-  url: {
-    API_URL: ``,
-  },
 };
 
 export default process.env.NODE_ENV === `development` ? dev : prod;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import fs from "fs";
+import path from "path";
+
+const devConfig = {
+  server: {
+    // host: "https://test-rr.treibaer.de",
+  },
+};
+const prodConfig = {
+  server: {
+    host: "https://rt.treibaer.de",
+    https: {
+      key: fs.readFileSync(
+        path.resolve("/Users/hannes/tmp/treibaer.de", "privkey.pem")
+      ),
+      cert: fs.readFileSync(
+        path.resolve("/Users/hannes/tmp/treibaer.de", "cert.pem")
+      ),
+    },
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  ...(process.env.NODE_ENV === "development" ? devConfig : prodConfig),
+});
